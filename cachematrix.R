18875480e3aff2c97ad514310854966c8669573d
KkM Coursera <62966579+kkmcoursera@users.noreply.github.com>
2020-04-04 19:06:31 -0400

    The shell assignment, folked and cloned from Prof. Peng, has been modifed to achieve the assignment goal, to produce a cached version of the solve function, called cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The original solve(a, ...) function, when given a single argument, a,
+## is a generic function solves the equation a %*% x = b for x, 
+## where b is the identity matrix.  The result returned is an inverse of a.
 
-## Write a short comment describing this function
+## In the case of makeCacheMatrix, we are looking to create local overrides
+## to the basic internal functions within the solve() function.  By doing so,
+## the overridden functions will support our new cached version of solve(), 
+## thus allowing us to avoid the added compute needed to invert matrices, 
+## if we had already computed such an invertion.
+## This approach has been modelled after the makeVector and cacheMean samples.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This new function, cacheSolve, leverages the workings of the solve() function,
+## but before executing the matrix inversion associated with a single argument
+## call (to solve internally), instead the function will first check in cache,
+## to see if we've already performed the inversion of such argument.
+## When found in cache, the cacheSolve function will instead use the calculated
+## values stored in the cache, and return that value instead.  However, if not
+## found in cache, the function will call solve(), and then store the calculated
+## matrix value in cache (to use another time) before returning the inverted
+## matrix to the caller.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## cacheSolve will return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
